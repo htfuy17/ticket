c3988d939bb9580d79cd1d005ceb9dbfc56cae6e
htfuy17 <cpf871117@gmail.com>
2019-01-19 02:21:45 +0800

    2019/01/19

--- ticket/account/urls.py
+++ ticket/account/urls.py
@@ -3,5 +3,6 @@ from account import views
 
 app_name = 'account'
 urlpatterns = [
-
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
--- ticket/account/views.py
+++ ticket/account/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 
-# Create your views here.
+from account.forms import UserForm
+
+
+def register(request):
+    '''
+    Register a new user
+    '''
+    template = 'account/register.html'
+    if request.method == 'GET':
+        return render(request, template, {'userForm':UserForm()})
+
+    # POST
+    userForm = UserForm(request.POST)
+    if not userForm.is_valid():
+        return render(request, template, {'userForm':userForm})
+
+    userForm.save()
+    messages.success(request, '歡迎註冊')
+    return redirect('main:main')
+
+
+def login(request):
+    '''
+    Login an existing user
+    '''
+    template = 'account/login.html'
+    if request.method == 'GET':
+        return render(request, template)
+
+    # POST
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    if not username or not password:    # Server-side validation
+        messages.error(request, '請填資料')
+        return render(request, template)
+
+    user = authenticate(username=username, password=password)
+    if not user:    # authentication fails
+        messages.error(request, '登入失敗')
+        return render(request, template)
+
+    # login success
+    auth_login(request, user)
+    messages.success(request, '登入成功')
+    return redirect('main:main')
+
